75a2f95847caa41307b792e631b8e29b276f3cd4
Darlene-M <moundoungadarlene@gmail.com>
2024-03-10 19:40:40 +0100

    Add sections of the page

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+function Header () {
+    return (
+        <div>
+            <div>
+                <h1>Darlène Moundounga</h1>
+            </div>
+            <div>
+                <p>A propos</p>
+                <p>Compétences</p>
+                <p>Projets</p>
+                <p>Contactez-moi</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
