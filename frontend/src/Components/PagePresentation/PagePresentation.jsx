5ce5acc10aa0e926a1f75d2bfeb5ade0b0b6d411
Darlene-M <moundoungadarlene@gmail.com>
2024-03-10 21:04:25 +0100

    Add title & text of pagePresentation

---
+++ frontend/src/Components/PagePresentation/PagePresentation.jsx
@@ -0,0 +1,11 @@
+function PagePresentation() {
+    return(
+        <div>
+            <h1>Darlène Moundounga- Portfolio</h1>
+            <p>Bonjour et bienvenue sur mon Portfolio</p>
+            <p>Développeuse Web Front-end</p>
+        </div>
+    )
+}
+
+export default PagePresentation
