c2a27631c9b4264e1768f09ce8b3abe0516eea46
Darlene-M <moundoungadarlene@gmail.com>
2024-03-13 14:51:12 +0100

    Add sccss of form

--- frontend/src/Components/Form/Form.jsx
+++ frontend/src/Components/Form/Form.jsx
@@ -1,15 +1,15 @@
 function Form () {
     return(
-        <div>
+        <div className="contact">
             <h3>Contactez moi !</h3>
-            <div>
-                <label htmlFor="name">Nom</label>
-                <input type="text" name="name" id="name" placeholder="Votre nom"/>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Votre adresse mail" />
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
+            <div className="form">
+                <div className="infos">
+                    <input type="text" name="name" id="name" placeholder="Nom"/>
+                    <input type="email" name="email" id="email" placeholder="Adresse-mail" />
+                </div>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
             </div>
+            <button>Envoyer</button>
         </div>
     )
 }
