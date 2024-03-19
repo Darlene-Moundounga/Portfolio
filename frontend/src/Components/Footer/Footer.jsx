67f86e7e4cc00938adb324fd0e3b93c9e460ba77
Darlene-M <moundoungadarlene@gmail.com>
2024-03-19 22:14:45 +0100

    finish responsive

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return(
+        <div className="footer">
+            <div className="linkToNetworks">
+                <a href="https://github.com/Darlene-Moundounga">
+                    <img src="https://cdn-icons-png.flaticon.com/128/1051/1051326.png" alt="Lien vers le profil Github de Darlène" />
+                </a>
+                <a href="https://www.linkedin.com/in/darlene-moundounga/">
+                    <img src="https://cdn-icons-png.flaticon.com/128/3938/3938044.png" alt="Lien vers le profil Linkedin de Darlène" />
+                </a>
+            </div>
+            <div className="copyright"><p><img src="https://cdn-icons-png.flaticon.com/128/5009/5009849.png" alt="" /> Tous droits réservés </p></div>
+        </div>
+
+    )
+}
+export default Footer
