9b21e119fed3a2447cbc3c1473d6d2ec1b673492
Darlene-M <moundoungadarlene@gmail.com>
2024-03-27 14:30:41 +0100

    Change text shadow title

--- frontend/src/Layout/Layout.jsx
+++ frontend/src/Layout/Layout.jsx
@@ -5,9 +5,9 @@ import Footer from '../Components/Footer/Footer'
 function Layout ( {children} ) {
     return (
     <React.Fragment>
-            <Header/>
-            {children}
-            <Footer/>
+        <Header/>
+        {children}
+        <Footer/>
     </React.Fragment>
     )
 }
